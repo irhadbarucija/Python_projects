991a5deb82dee2dcb46fc53ec7c2d40ed6e5b7f8
irhad_barucija <irhad.barucija@gmail.com>
2025-11-20 13:40:42 +0100

    Add Naruto Quiz project

---
+++ 1_Naruto_Shippuden_Quiz/main.py
@@ -0,0 +1,141 @@
+# ASCII ART TO START
+print('''
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡼⢹⡆⠀⠀⠀⠀⣠⠂⣀⣤⠖⣻⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣤⠀⠀⠀⠀⣶⣤⠏⠀⠀⢇⠀⣀⡴⠻⠷⠚⠉⠀⢠⠏⠀⠀⠀⠀⣀⣀⣀⣀⣀⣤⡶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠉⢇⠀⢀⡾⠛⠁⠀⠀⠀⠘⠛⠁⠀⠀⠀⠀⠀⠀⠾⠔⠚⠛⠋⠉⠁⠀⠀⠀⣠⠞⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⢰⠇⠀⠘⣦⡜⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡼⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⠀⢸⠛⢦⡀⠀⠀⡏⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⢯⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠
+                ⠀⠀⠀⠀⠘⡇⠀⠙⢦⣼⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠳⠦⣄⣀⣀⡄⠀⠀⠀⠀⠀⠀⠀⣰⣾⣿
+                ⠀⠀⠀⠀⠀⣿⠀⠀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⠴⠋⠀⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿
+                ⠀⠀⠀⠀⠀⢸⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣛⡶⠖⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⠟
+                ⠘⢶⡒⠒⠒⠊⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢿⡀⠀⠀⠀⠀⢀⣴⣿⣿⣿⣿⡿⢃⣀
+                ⠀⠀⠙⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣆⠀⠀⠀⠀⠀⠀⠈⢳⣄⠀⢀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿
+                ⠀⠀⠀⠀⣹⠀⠀⣤⣀⣀⠰⣄⠀⠀⠀⠀⠀⢸⡀⠀⠀⢹⡄⠀⠀⠀⠀⠀⠀⢀⣀⠀⠀⠀⠀⠀⠙⡆⠀⠀⠀⣄⣀⣀⣠⣭⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟
+                ⠀⠀⠀⢠⡇⠀⢰⣿⣿⣿⣿⣿⣦⠀⠀⠀⠀⠀⣧⠀⠀⠀⢿⣶⣶⣤⣄⣀⣀⡀⠙⠻⣿⣶⣶⣤⣀⣹⣷⣶⣤⣹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠁⠀⠀
+                ⠀⠀⠀⢸⠁⢠⡿⠿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⢹⡄⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣷⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠛⠛⠋⠉⠀⠀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⡏⢠⠋⡇⢠⣆⠈⢩⡉⠙⠛⢹⣆⠀⠀⠈⣿⣦⡀⠀⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣥⣤⡤⢤⡶⠂⠀⠀⠀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⣷⠏⠀⣧⡏⣽⣷⡟⠁⠆⠀⠀⠙⣦⠀⠀⢿⣿⣿⣦⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠀⠀⢀⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                ⠀⠀⢸⠏⠀⠀⣿⣧⣿⡇⠀⠀⣾⡿⠀⣴⣿⣷⡀⢸⣿⣿⣿⣿⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠋⠀⠀⠀⣶⣿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⠀⠀⠀⢹⠿⢯⡇⠀⠄⠘⡇⠀⠀⢹⣿⣿⣄⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿⠟⣩⢯⠿⡄⠀⠀⢶⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⠀⠀⠀⠈⢿⣦⣤⣀⣀⠘⠁⠀⠰⢾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⠁⡼⢁⡾⢿⡜⣦⣷⠀⠀⠘⣯⣿⣿⣷⣤⣀⠀⠀⠀⠀⠀⠀⠀
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠻⢿⣿⣿⣿⣷⣶⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⢛⣛⠛⠛⠙⣏⠀⠀⠀⢠⠃⣿⢻⣦⣀⢸⣽⠀⠀⠀⢘⣿⣿⣿⣿⣿⣷⣦⣤⣄⡀⠀⠀
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢷⡾⣅⣀⣠⣴⣶⣿⣿⡿⠷⢶⡀⠀⠀⣿⣿⣧⠰⠆⠸⣆⠀⠀⡿⠀⢻⣟⣿⣿⡿⣿⢧⣼⣶⣿⣿⣿⡿⣿⣿⣿⣿⣿⣿⣿⣿⣶
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣇⠈⣻⠟⢹⡟⢱⣿⡇⠀⡾⠁⠀⠰⣿⣿⣷⠈⣿⣷⣿⣆⢰⠃⢀⣼⡿⠋⠈⣼⠋⠀⣠⣿⣿⣿⣿⣧⣿⣿⣿⣿⣿⣿⣿⣿⣿
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡟⠘⣡⠴⠦⠾⠷⣿⣤⡾⠏⡀⠀⠘⣿⣿⣿⣀⣈⣀⣀⣹⡾⠀⠀⠀⠀⢀⣼⡻⠀⢸⣿⣿⣿⣿⣿⣟⣿⣿⣿⣿⣿⣿⣿⣿⣿
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⠇⠀⠀⠀⠀⣄⣀⣀⣀⡀⠐⠓⠊⠉⠉⢻⡏⠉⡉⠉⠉⢉⣡⡄⢠⣤⣾⣿⡿⢃⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⡏⠀⠀⠀⠀⠀⠈⠉⠙⠹⠉⠀⠒⠒⠒⠶⢼⡧⣬⣽⣄⠀⠠⠟⠁⣾⣿⣾⣿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣀⠀⣀⣠⣤⡴⠀⠀⠀⠀⠀⠀⠀⠲⠦⣄⣸⡇⠀⠀⢹⡀⠀⠉⢠⣷⢫⡿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⣿⡟⠁⠀⠀⠀⠀⠈⢑⣦⣀⡀⠀⢸⠋⠳⣄⠀⠀⠀⢀⣿⣳⢟⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠒⡦⣤⡴⠾⠿⣩⠭⢵⣾⠇⠀⡾⠀⣧⠄⠀⢀⣴⡟⣿⡇⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣌⣙⠓⠒⠒⠚⠋⠁⢀⣾⠃⠀⠀⣠⡴⠋⠉⣿⠟⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢻⠉⠃⠀⠀⠀⠀⣼⠃⢀⣠⣾⣿⣤⣔⡀⢐⣴⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣦⡀⠀⠀⢀⣾⣣⣶⣿⣿⣿⣿⣿⣿⢿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+                ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃
+''')
+print('''
+ _____                                                                                                    _____ 
+( ___ )--------------------------------------------------------------------------------------------------( ___ )
+ |   |                                                                                                    |   | 
+ |   |     _        _______  _______          _________ _______    _______          _________ _______     |   | 
+ |   |    ( (    /|(  ___  )(  ____ )|\     /|\__   __/(  ___  )  (  ___  )|\     /|\__   __// ___   )    |   | 
+ |   |    |  \  ( || (   ) || (    )|| )   ( |   ) (   | (   ) |  | (   ) || )   ( |   ) (   \/   )  |    |   | 
+ |   |    |   \ | || (___) || (____)|| |   | |   | |   | |   | |  | |   | || |   | |   | |       /   )    |   | 
+ |   |    | (\ \) ||  ___  ||     __)| |   | |   | |   | |   | |  | |   | || |   | |   | |      /   /     |   | 
+ |   |    | | \   || (   ) || (\ (   | |   | |   | |   | |   | |  | | /\| || |   | |   | |     /   /      |   | 
+ |   |    | )  \  || )   ( || ) \ \__| (___) |   | |   | (___) |  | (_\ \ || (___) |___) (___ /   (_/\    |   | 
+ |   |    |/    )_)|/     \||/   \__/(_______)   )_(   (_______)  (____\/_)(_______)\_______/(_______/    |   | 
+ |___|                                                                                                    |___| 
+(_____)--------------------------------------------------------------------------------------------------(_____)
+''')
+
+# COLORS
+RED = "\033[31m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+BLUE = "\033[34m"
+CYAN = "\033[36m"
+MAGENTA = "\033[35m"
+RESET = "\033[0m"
+
+# Player score
+score = 0
+
+# Ask player to start or quit the quiz
+welcome = input(f"Welcome to the Naruto Quiz! There are 10 questions in total.\nTo start playing press 's', to quit anytime press 'q'. ").lower()
+
+# If the player chooses to start the quiz
+if welcome == "s":
+
+    # Tuple that contains: question text, list of accepted correct answers, and explanation to show after answering
+    questions = [
+        ("From who did Kakashi get his eye from?", ["obito"], 
+        "He lost an eye, and Obito's body was crushed and he gave his eye to Kakashi."),
+
+        ("What jutsu is Naruto most proficient in?", ["shadow clone", "shadow clone jutsu"], 
+        "He is most proficient in the Shadow Clone Jutsu, having made it a central part of his Taijutsu and Ninja skills in general."),
+
+        ("What does Naruto wear before he became a Genin?", ["goggles", "glasses"], 
+        "This was before Iruka Sensei passed him on his test. Iruka actually gave him the one off of his own head!"),
+
+        ("Which Tailed-Beast is sealed inside Naruto?", ["nine tailed beast", "the fox", "kurama"], 
+        "Naruto is the Jinchuriki of the Nine-Tailed Fox. It was placed there by The Fourth Hokage when the the Nine-Tailed Fox attacked Konoha."),
+
+        ("What is Naruto's favorite food?", ["ramen", "ichiraku ramen"], 
+        "You can usually find Naruto eating ramen at the Ichiraku Ramen Shop. Either way, he hates vegetables."),
+
+        ("What is the name of Tailed-Beast that is sealed inside Gaara?", ["one tailed beast", "shukaku"], 
+        "The Kazekage (Gaara's father) used a jutsu to seal a sand incarnate within Gaara before he was born. "
+        "That way Gaara can become a powerful ninja."),
+
+        ("Which Hokage sealed the Nine-Tailed fox inside Naruto?", ["fourth hokage", "minato"], 
+        "A certain shinobi sealed the beast within a boy (Naruto). "
+        "He saved the village, but gave his life in the process."),
+
+        ("What is the highest level of ninja in the Hidden Leaf Village?", ["hokage"], 
+        "Hokage means Fire Shadow, the title given to the strongest ninja in Konoha Sato no Kakure, also known as the Hidden Leaf Village."),
+
+        ("Who is Naruto's rival?", ["sasuke", "sasuke uchiha"], 
+        "Uchiha Sasuke is one of two surviving Uchiha Clan members."),
+
+        ("Who was a student of the Fourth Hokage?", ["kakashi", "kakashi hatake"], 
+        "Kakashi was in a squad with an Uchiha whom he got his Sharingan from. "
+        "The Sharingan was transplanted when Kakashi lost and eye and his friend was about to die in battle.")
+    ]
+
+    # MAIN QUIZ LOOP
+    # enumerate() gives an index number to each question
+    for index, (question, answers, explanation) in enumerate(questions, start=1):
+        user_answer = input(f"\n{YELLOW}{index}{RESET}. {CYAN}{question}{RESET} (type 'q' to quit): ").lower()
+
+        # Quit the quiz mid-game
+        if user_answer == "q":
+            print("\nYou quit the quiz!")
+            break
+
+        # If answer is correct
+        if user_answer in answers:
+            score += 1 # Add a point
+            print(f"\n{GREEN}Correct!{RESET}")
+            print(f"\n{YELLOW}{explanation}{RESET}") # Print explanation
+        else:
+            print(f"\n{RED}Wrong!{RESET}")
+
+    # FINAL RANKING
+    if score > 0: # Only show ranking if at least 1 question was answered
+        print(f"\n{RED}--- FINAL RANK ---{RESET}")
+        if score <= 4:
+            print(f"You scored {RED}{score}{RESET}. You are {RED}Genin!")
+        elif score <= 7:
+            print(f"You scored {RED}{score}{RESET}. You are {RED}Chunin!")
+        else:
+            print(f"You scored {RED}{score}{RESET}. You are {RED}Jonin!")
+
+# If player chooses to quit before starting
+elif welcome == "q":
+    print("You quit the quiz!")
+
+# Wrong input (not s or q)
+else:
+    print("Invalid input, try again!")
